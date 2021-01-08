604c9da9c0d6acf873ae801f02fa47ecd5eda9a6
CVAS7 <suresh.malasani@gmail.com>
2021-01-08 07:55:23 +0400

    with one Funcation at the end as inventory_report

--- Work/report.py
+++ Work/report.py
@@ -7,35 +7,26 @@ def read_inventory(filename):
         inventory = []
         for r in rows:
             product = dict(zip(header, r))
-#            print(product)
             product['quant'] = int(product['quant']) # updating the data type
             product['price'] = float(product['price']) # updating the date type
             inventory.append(product)
-#    print(inventory)
     return inventory
-#    print(inventory)
-#read_inventory('Data/inventory.csv')
 
 def read_prices(filename):
     with open(filename,'rt') as FH:
         rows = csv.reader(FH)
-        dprices = {}
+        prices = {}
         for line in rows:
             try:
-                dprices[line[0]] = float(line[1])
+                prices[line[0]] = float(line[1])
             except IndexError:
                 continue
-#    print(dprices)
-#    print('dprices')
-    return dprices
-
-
+    return prices
 def make_report(inventory,prices):
     report = []
     for i in inventory:
-        row = (i['name'],i['quant'],latest[i['name']],(latest[i['name']]-i['price'])) # This should be price or latest also works
+        row = (i['name'],i['quant'],prices[i['name']],(prices[i['name']]-i['price'])) # This should be price or latest also works
         report.append(row)
-#    print('report',report)
     return report
 def print_report(report):
     headers = ('Name', 'Quantity', 'Price', 'Change')
@@ -45,22 +36,10 @@ def print_report(report):
     for row in report:
             print('{:>10s} {:>10d} {:>10.2f} {:>10.2f}'.format(*row)) # what happens if length of name is more than 10 char
 #read_prices('Data/prices.csv')
-filename = 'Data/inventory.csv'
-inventory = read_inventory(filename)
-"""
-total = 0.0
-for i in inventory:
-    total += i['quant'] * i['price']
-#print('Total Cost',total)
-"""
-filename = 'Data/prices.csv'
-latest = read_prices(filename)
-"""
-new_total = 0.0
-for j in inventory:
-    new_total += j['quant'] * latest[j['name']]
+def inventory_report(inventory_filename,price_filename):
+    inventory = read_inventory(inventory_filename)
+    latest = read_prices(price_filename)
+    report = make_report(inventory,latest)
+    print_report(report)
 
-print('Total Gain',new_total-total)
-"""
-report = make_report(inventory,latest)
-print_report(report)
+inventory_report('Data/inventory.csv','Data/prices.csv')
