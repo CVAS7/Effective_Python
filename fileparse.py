3066d8da90c6df058672ae8d571bc2385f517186
CVAS7 <suresh.malasani@gmail.com>
2021-01-12 08:54:48 +0400

    For different delimiters

--- fileparse.py
+++ fileparse.py
@@ -1,21 +1,31 @@
 import csv
-def parse_csv(filename, select=['name','quant','price']):
+def parse_csv(filename, select=None, types=None, has_headers=True,delimiter=','):
     with open(filename) as f:
-        rows =csv.reader(f)
-        headers = next(rows)
-        if select:
-            indices = [headers.index(colname) for colname in select]
-            headers = select
-        else:
-            indices = []
+        rows =csv.reader(f,delimiter = delimiter)
+        if has_headers:
+            headers = next(rows)
+
+            if select:
+                indices = [headers.index(colname) for colname in select]
+                headers = select
+            else:
+                indices = []
 
         records = []
         for row in rows:
             if not row :
                 continue
-            if indices:
+
+            if select:
                 row = [row[index] for index in indices]
-            record = dict(zip(headers,row))
+
+            if types:
+                row = [func(value) for func, value in zip(types, row)]
+
+            if has_headers:
+                record = dict(zip(headers,row))
+            else:
+                record = tuple(row)
             records.append(record)
 
     return records
