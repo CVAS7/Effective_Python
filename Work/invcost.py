a03ba36145d567dd4a364acb3915de0130a3b086
CVAS7 <suresh.malasani@gmail.com>
2021-01-20 06:50:37 +0400

    using import read_inventory from report.py

--- Work/invcost.py
+++ Work/invcost.py
@@ -1,20 +1,14 @@
 import csv
 import sys 
+from report import read_inventory
+
 def inventory_cost(filename):
-    Total = 0
-    
-    with open(filename,'rt') as FH:
-        rows = csv.reader(FH)
-        headers = next(rows)
-        for idx,row in enumerate(rows,start=1):
-            record = dict(zip(headers, row))
-            try:
-                quant = int(record['quant'])
-                price = float(record['price'])
-                Total += quant*price
-            except ValueError:
-                print(f"Row {idx} :Bad Row {row}")
+    ive = read_inventory(filename)
+    Total = 0.0
+    for prod in ive:
+        Total += prod['quant']*prod['price']
     return Total            
+
 try:
     filename = sys.argv[1]
 except IndexError:
