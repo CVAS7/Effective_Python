9e696314488a05f694283577fbaaddf41d33f23f
CVAS7 <suresh.malasani@gmail.com>
2021-01-20 07:58:30 +0400

    Update invcost.py

--- Work/invcost.py
+++ Work/invcost.py
@@ -1,5 +1,5 @@
 import csv
-import sys 
+import sys
 from report import read_inventory
 
 def inventory_cost(filename):
@@ -7,12 +7,19 @@ def inventory_cost(filename):
     Total = 0.0
     for prod in ive:
         Total += prod['quant']*prod['price']
-    return Total            
+    return Total
 
-try:
-    filename = sys.argv[1]
-except IndexError:
-    print('Filename is not given in the arugment, filename taken as Default Data/inventory.csv')
-    filename = 'Data/inventory.csv'
-cost = inventory_cost(filename)
-print('Total cost:',cost)
+def main(argv):
+    if len(argv) != 2:
+        raise SystemExit(f'Usage: {argv[0]} {inventory.csv}')
+
+    try:
+        filename = argv[1]
+    except IndexError:
+        print('Filename is not given in the arugment, filename taken as Default Data/inventory.csv')
+    cost = inventory_cost(filename)
+    print('Total cost:',cost)
+
+if __name__ == "__main__":
+    import sys
+    main(sys.argv)
