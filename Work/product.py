c809090ae5bac0264dcdd3a29ace29269f49c35a
CVAS7 <suresh.malasani@gmail.com>
2021-01-22 08:00:17 +0400

    with cost and sell

--- Work/product.py
+++ Work/product.py
@@ -1,5 +1,26 @@
+""" 
+Define a prodcut class
+"""
 class Product:
+    """ 
+    Class to repreent a product consisting of name, quant, price
+    """
     def __init__ (self, name, quant, price):
-            self.name = name
-            self.quant = quant
-            self.price = price
+        """
+        Returns the cost product
+        """
+        self.name = name
+        self.quant = quant
+        self.price = price
+
+    def cost(self):
+        """
+        Returns the cost product
+        """
+        return self.quant*self.price
+    
+    def sell(self, sold):
+        """
+        Sell few units of the prdduct
+        """
+        self.quant = self.quant - sold
