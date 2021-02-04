332bb76e0506b4baaa8553c0d495b4918aac2692
CVAS7 <suresh.malasani@gmail.com>
2021-02-04 07:56:13 +0400

    with formats

--- Work/ticker.py
+++ Work/ticker.py
@@ -1,5 +1,7 @@
 from follow import follow
 import csv
+import report
+import tableformat 
 
 def convert_types(rows, types):
     for row in rows:
@@ -8,6 +10,11 @@ def convert_types(rows, types):
 def make_dicts(rows, headers):
     for row in rows:
         yield dict(zip(headers, row))
+
+def filter_names(rows, names):
+    for row in rows:
+        if row['name'] in names:
+            yield row
 
 def select_columns(rows, indices):
     for row in rows:
@@ -19,6 +26,20 @@ def parse_product_data(lines):
     rows = convert_types(rows, [str,float,float])
     rows = make_dicts(rows, ['name', 'price', 'change'])
     return rows
+
+
+def ticker(filename, logfile, fmt):
+    inventory = report.read_inventory(filename)
+    rows = parse_product_data(follow(logfile))
+    rows = filter_names(rows, inventory)
+    formatter = tableformat.create_formatter(fmt)
+    formatter.headings(['Name','Price','Change'])
+    for row in rows:
+        name = row['name']
+        price = row['price']
+        change = row['change']
+        rowdata = [name, f'{price:0.2f}', f'{change:0.2f}']
+        formatter.row(rowdata)
 
 if __name__ == '__main__':
 
